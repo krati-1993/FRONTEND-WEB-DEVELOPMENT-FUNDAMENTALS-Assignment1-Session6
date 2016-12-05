0f049d185cc1b0ec95f67bef1672ced992c1b6ac
krati-1993 <kratikavarshney2011@gmail.com>
2016-12-05 17:32:01 +0530

    Add files via upload

---
+++ appcir.js
@@ -0,0 +1,5 @@
+		
+		var radius = 5; //gets the value from variable 
+        console.log("The area of the circle is " + (radius * radius * Math.PI)); //print the area of circle on console.
+		
+		
